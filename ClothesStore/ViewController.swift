5df20c5316a72116e35cafa08f1a7b2169e134d2
Zeyad Khaled Sultan <89984601+ZeyadSultan@users.noreply.github.com>
2022-08-13 11:38:13 +0200

    coreDataFns + logIn and SignUp Screen
    core data functions missing getting user data by id

--- ClothesStore/ViewController.swift
+++ ClothesStore/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+var userId:Int = 0
+
+func showAlert(msg:String, vc:UIViewController)
+{
+    let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    vc.present(alert,animated: true,completion: nil)
+}
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailLogIn: UITextField!
+    @IBOutlet weak var passwordLogIn: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Hi")
+        
+        
+        
     }
+    @IBAction func logInButton(_ sender: Any) {
+         guard let email = emailLogIn.text, let pass = passwordLogIn.text, email != "" && pass != ""
+        else{ showAlert(msg: "Please enter your email & password!",vc:self)
+                 return
+         }
+         guard let email = emailLogIn.text, email != ""
+             else{ showAlert(msg: "Please enter your email!",vc:self)
+                 return
+         }
+         guard let pass = passwordLogIn.text, pass != ""
+         else{ showAlert(msg: "Please enter your password!",vc:self)
+             return
+         }
+        
+        if userExists(email: email, password: pass) == -1
+        {
+            showAlert(msg: "Your email or password are'nt correct try again!",vc:self)
+        }
+        else
+        {
+            // move to the menu screen with user's Id
+            
+        }
+    }
+    
+    
+    
+    
 
-
+        
+        
 }
 
